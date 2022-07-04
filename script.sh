fa09cd1f8d0161de12ea6ffa7df0648292df01de
dsw47258 <dsw47258@student.dsw.edu.pl>
2022-07-04 22:48:43 +0200

    Skrypt automatycznie tworzy 100 plików logx.txt

--- script.sh
+++ script.sh
@@ -5,3 +5,13 @@ if [ "$1" == "--date" ];
 then
 echo $today
 fi
+if [ "$1" == "--logs" ];
+then
+b=1--
+for i in $(seq 100)
+do 
+    echo log$i >> log$i.txt
+    echo $0 >> log$i.txt
+    echo $Today >> log$i.txt
+done
+fi
