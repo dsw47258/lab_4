f3f5f9d68d411d7a9fdf12e65c3e4006a83de36f
dsw47258 <dsw47258@student.dsw.edu.pl>
2022-07-04 23:09:23 +0200

    Cz.2, myślnik1

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$1" == "--help" ];
+if [ "$1" == "--help" ] || [ "$1" == "-h" ];
 then
 echo " Dostępne opcje: "
 echo "--date wyświetla aktualną datę"
@@ -7,18 +7,18 @@ echo "--logs X Tworzy X plików o nazwie logX.txt i wpisuje do nich dane"
 echo "--logs Powtarza powyższy punkt ze 100 plikami"
 fi
 Today=$(date_
-if [ "$1" == "--date" ];
+if [ "$1" == "--date" ] || [ "$1" == "-d" ];
 then
 echo $today
 fi
-if [ "$1" == "--logs" ] && [ -n "$2" ];
+if [ "$1" == "--logs" ] || [ "$1" == "-l" ] && [ -n "$2" ];
 then for i in $(seq $2)
 do
     echo log$i >> log$i.txt
     echo $0 >> log$i.txt
     echo $Today >> log$i.txt
 done
-elif [ "$1" == "--logs" ]; 
+elif [ "$1" == "--logs" ] || [ "$1" == "-l" ]; 
 then
 for i in $(seq 100)
 do 
