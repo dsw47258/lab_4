8629a48bfa636ea6bbf937a6f1c10f8a86cfadc8
dsw47258 <dsw47258@student.dsw.edu.pl>
2022-07-04 22:54:50 +0200

    Tworzenie automatycznie 30 plików

--- script.sh
+++ script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-
 Today=$(date_
 if [ "$1" == "--date" ];
 then
 echo $today
 fi
-if [ "$1" == "--logs" ];
+if [ "$1" == "--logs" ] && [ -n "$2" ];
+then for i in $(seq $2)
+do
+    echo log$i >> log$i.txt
+    echo $0 >> log$i.txt
+    echo $Today >> log$i.txt
+done
+elif [ "$1" == "--logs" ]; 
 then
-b=1--
 for i in $(seq 100)
 do 
     echo log$i >> log$i.txt
     echo $0 >> log$i.txt
     echo $Today >> log$i.txt
 done
-fi
+fi 
